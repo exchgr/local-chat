a9c3be38fd01d7e73b6dc34d20398f788f4764d2
Koushik Dasika <kdasika@koushikdasika.com>
2012-05-19 18:26:24 -0400

    added simpleform gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 LocalChat::Application.routes.draw do
-  resources :users
+  get "home/index"
 
+  resources :users
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
