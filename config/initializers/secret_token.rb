0418def8db8f39229b07dbb40931de3ab087b08a
Koushik Dasika <kdasika@koushikdasika.com>
2012-05-19 15:20:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalChat::Application.config.secret_token = 'ae3850105a64d7d4851efee7005e7fb514745dfd7275bffeac53060cd042719f6683462195e2b34e78bb3dcd079655f3a0cb47db646a4a8cd371281a3f03b17d'
