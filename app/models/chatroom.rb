5072ff44797c60fd7f155072380b762a029c041c
Koushik Dasika <kdasika@koushikdasika.com>
2012-05-19 17:15:18 -0400

    Updated models so that they show the relationships between the various models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,7 @@
 class Chatroom < ActiveRecord::Base
   attr_accessible :lat, :long, :name, :roomnumber
+
+  has_many :users
+  has_many :messages, :through => :users
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
   attr_accessible :chatroom_id, :message, :user_id
+
+  belongs_to :user
+  belongs_to :chatroom
+
+
 end
