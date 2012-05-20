916c29837abdbd3b909bd014f7f4c5135453433b
Koushik Dasika <kdasika@koushikdasika.com>
2012-05-19 20:47:32 -0400

    Fixed the routing for the index page. Checks to see if you are registered. If you are, then you can join a chatroom.  If you are not, it sends you to the user signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :sessionExist
+
+  def sessionExist
+	  if session[:user_id].nil?
+		  redirect_to new_user_path
+	  end
+  end
 end
